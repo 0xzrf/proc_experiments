10d4c889a5f3444dbee28b2c40f402ab8644637b
Zeref <zeref18.drgnl@gmail.com>
2026-03-29 15:04:30 +0530

    added repetition macro

---
+++ macs/src/dec_macs/repetition.rs
@@ -0,0 +1,19 @@
+////////// DO NOT CHANGE BELOW HERE /////////
+fn print_success() {
+    println!("Yay, the if statement worked.");
+}
+////////// DO NOT CHANGE ABOVE HERE /////////
+
+macro_rules! if_any {
+    ($($is_true:expr),*; $execute:block) => {
+            if $($is_true)||+ $execute
+    };
+}
+
+////////// DO NOT CHANGE BELOW HERE /////////
+
+fn main() {
+    if_any!(false, 0 == 1, true; {
+        print_success();
+    })
+}
