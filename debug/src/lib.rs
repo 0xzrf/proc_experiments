22ad85d48cadc580f53d90c6e3ec4d3cef9efaf3
Zeref <zeref18.drgnl@gmail.com>
2026-03-31 15:06:49 +0530

    added typed parameter restriction for debug

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Attribute, DeriveInput};
+use syn::{parse_macro_input, parse_quote, Attribute, DeriveInput, GenericParam, Generics};
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -22,6 +22,10 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 .into();
         }
     };
+
+    // only allow implementing this macro if the typed parameters implement std::fmt::Debug
+    let generics = add_trait_bounds(input.generics);
+    let (impl_generic, type_generic, where_clause) = generics.split_for_impl();
 
     let mut field_stmts = Vec::new();
     for (i, field) in fields.iter().enumerate() {
@@ -65,7 +69,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     }
 
     quote! {
-        impl std::fmt::Debug for #name {
+        impl #impl_generic std::fmt::Debug for #name #type_generic #where_clause {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                 f.write_str(#name_str)?;
                 f.write_str(" { ")?;
@@ -121,4 +125,13 @@ fn extract_debug_format(attrs: &[Attribute]) -> Result<Option<syn::LitStr>, syn:
     }
 
     Ok(found)
+}
+
+fn add_trait_bounds(mut generics: Generics) -> Generics {
+    for param in &mut generics.params {
+        if let GenericParam::Type(ref mut type_param) = *param {
+            type_param.bounds.push(parse_quote!(std::fmt::Debug));
+        }
+    }
+    generics
 }
