f5ff448df71368978419b1a3589e2aa94d5e76d3
Zeref <zeref18.drgnl@gmail.com>
2026-04-02 21:14:59 +0530

    added initial commit for sorted proc_macro

---
+++ sorted/src/lib.rs
@@ -0,0 +1,10 @@
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, Item};
+
+#[proc_macro_attribute]
+pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
+    let _ = args;
+    let input = parse_macro_input!(input as Item);
+
+    args
+}
